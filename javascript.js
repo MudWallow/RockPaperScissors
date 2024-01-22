8cf55ca6356cdf8f738a335a3355c6121576739f
MudWallow <153695981+MudWallow@users.noreply.github.com>
2024-01-22 09:29:56 -0700

    Add system for player to choose rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
-console.log("test");
+
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+let playerChoice;
+rockButton.addEventListener("click", choseRock);
+paperButton.addEventListener("click", chosePaper);
+scissorsButton.addEventListener("click", choseScissors);
+console.log(playerChoice);
+
+if (playerChoice == "Rock") {
+    choseRock;
+} else if (playerChoice == "Paper") {
+    chosePaper;
+} else if (playerChoice == "Scissors") {
+    choseScissors;
+}
+
+function choseRock () {
+    console.log("Rock");
+}
+
+function chosePaper () {
+    console.log("Paper");
+}
+
+function choseScissors () {
+    console.log("Scissors");
+}
