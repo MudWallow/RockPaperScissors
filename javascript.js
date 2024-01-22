673179bdedebf59d433a3add6809b9166914f6a9
MudWallow <153695981+MudWallow@users.noreply.github.com>
2024-01-22 11:31:39 -0700

    Add choice comparison to decide who wins

--- javascript.js
+++ javascript.js
@@ -3,28 +3,45 @@
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
+const gameText = document.querySelector('.gameText');
 let playerChoice;
+let compChoice;
 rockButton.addEventListener("click", choseRock);
 paperButton.addEventListener("click", chosePaper);
 scissorsButton.addEventListener("click", choseScissors);
 console.log(playerChoice);
 
-if (playerChoice == "Rock") {
-    choseRock;
-} else if (playerChoice == "Paper") {
-    chosePaper;
-} else if (playerChoice == "Scissors") {
-    choseScissors;
+function compare () {
+    if (playerChoice === compChoice) {
+        gameText.textContent = ("You both chose " + playerChoice + "! It's a draw.")
+    } else if (playerChoice === "Paper" && compChoice === "Rock") {
+        gameText.textContent = ("You chose " + playerChoice + ", they chose " + compChoice + "! You win!")
+    } else if (playerChoice === "Paper" && compChoice === "Scissors") {
+        gameText.textContent = ("You chose " + playerChoice + ", they chose " + compChoice + "! You lost!")
+    } else if (playerChoice == "Scissors" && compChoice === "Paper") {
+        gameText.textContent = ("You chose " + playerChoice + ", they chose " + compChoice + "! You win!")
+    } else if (playerChoice == "Scissors" && compChoice === "Rock") {
+        gameText.textContent = ("You chose " + playerChoice + ", they chose " + compChoice + "! You lose!")
+    }
+}
+
+function makePcChoice () {
+    compChoice = "Rock"
+    compare();
 }
 
 function choseRock () {
-    console.log("Rock");
+    playerChoice = "Rock";
+    makePcChoice();
+
 }
 
 function chosePaper () {
-    console.log("Paper");
+    playerChoice = "Paper";
+    makePcChoice();
 }
 
 function choseScissors () {
-    console.log("Scissors");
+    playerChoice = "Scissors";
+    makePcChoice();
 }
